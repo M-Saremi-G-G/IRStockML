261d23a573eb12a29088d9fb39f506695dc4b261
M-Saremi-G-G <112410056+M-Saremi-G-G@users.noreply.github.com>
2023-06-14 12:04:04 +0330

    Add files via upload

---
+++ LSTMs.py
@@ -0,0 +1,66 @@
+# Python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+
+# Data
+data = pd.read_csv('stock_prices.csv')
+
+# Preprocessing 
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+# Training and testing
+training_size = int(len(scaled_data) * 0.8)
+test_size = len(scaled_data) - training_size
+train_data = scaled_data[0:training_size, :]
+test_data = scaled_data[training_size:len(scaled_data), :]
+
+# Create training and testing
+def create_dataset(dataset, time_steps=1):
+    X, Y = [], []
+    for i in range(len(dataset) - time_steps - 1):
+        a = dataset[i:(i + time_steps), 0]
+        X.append(a)
+        Y.append(dataset[i + time_steps, 0])
+    return np.array(X), np.array(Y)
+
+time_steps = 60
+X_train, Y_train = create_dataset(train_data, time_steps)
+X_test, Y_test = create_dataset(test_data, time_steps)
+
+# Prepare the data for LSTM
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+# LSTM model
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+# Compiling
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Training
+model.fit(X_train, Y_train, epochs=50, batch_size=32)
+
+# LSTM on the test data
+predictions = model.predict(X_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Results
+plt.plot(data['Close'].values[training_size + time_steps + 1:], color='blue', label='Actual Stock Price')
+plt.plot(predictions, color='red', label='Predicted Stock Price')
+plt.title('Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
