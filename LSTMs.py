fdd08be1c3c021468faf28fd17a6977ba6e3afab
M-Saremi-G-G <112410056+M-Saremi-G-G@users.noreply.github.com>
2023-06-14 11:53:37 +0330

    Update LSTMs.py

--- LSTMs.py
+++ LSTMs.py
@@ -6,10 +6,10 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-# Load the data
+# Loading
 data = pd.read_csv('stock_prices.csv')
 
-# Preprocess the data
+# Preprocess
 scaler = MinMaxScaler()
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 
@@ -19,7 +19,7 @@ test_size = len(scaled_data) - training_size
 train_data = scaled_data[0:training_size, :]
 test_data = scaled_data[training_size:len(scaled_data), :]
 
-# Create the training and testing datasets
+# Create datasets
 def create_dataset(dataset, time_steps=1):
     X, Y = [], []
     for i in range(len(dataset) - time_steps - 1):
@@ -32,11 +32,11 @@ time_steps = 60
 X_train, Y_train = create_dataset(train_data, time_steps)
 X_test, Y_test = create_dataset(test_data, time_steps)
 
-# Reshape the data for LSTM
+# Preparing the data for LSTM
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-# Build the LSTM model
+# LSTM model
 model = Sequential()
 model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
 model.add(Dropout(0.2))
@@ -46,17 +46,17 @@ model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
 
-# Compile the model
+# Compiling
 model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Train the model
+# Training
 model.fit(X_train, Y_train, epochs=50, batch_size=32)
 
-# Make predictions on the test data
+# Predictions on the test data
 predictions = model.predict(X_test)
 predictions = scaler.inverse_transform(predictions)
 
-# Plot the results
+# Results
 plt.plot(data['Close'].values[training_size + time_steps + 1:], color='blue', label='Actual Stock Price')
 plt.plot(predictions, color='red', label='Predicted Stock Price')
 plt.title('Stock Price Prediction')
